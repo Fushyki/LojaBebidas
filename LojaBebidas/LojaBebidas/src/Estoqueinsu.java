e2ec3c70da9713372348a4f578f0bb3fe0afdc28
Fushyki <127150427+Fushyki@users.noreply.github.com>
2025-06-03 19:01:10 -0300

    Delete LojaBebidas directory

--- LojaBebidas/LojaBebidas/src/Estoqueinsu.java
+++
@@ -1,6 +0,0 @@
-// Classe de exceção personalizada para casos de estoque insuficiente
-public class Estoqueinsu extends Exception {
-    public Estoqueinsu(String mensagem) {
-        super(mensagem);
-    }
-}
